8d30f5ee4459aaefced00789c6c931aa594a4721
Huong Bui Vu <hbui-vu@lab3r2s13.42abudhabi.ae>
2024-02-27 16:20:24 +0400

    quit

--- Commands2.cpp
+++ Commands2.cpp
@@ -8,9 +8,15 @@
 //delete from pfdsMap
 //set change to 1 somehow?
 //make sure there's nothing to free in the client object!
+//need to send :sender_id QUIT :goodbye message -> 
+	//should we send it to other users in the channel? -> we need to do this if client exits without quit command
+
 
 Commands::QUIT()
 {
+	//send ERROR :client _nick quit
+		//we will need to wait for the message to this client to be sent first before we quit?
+		//maybe we should try to push this message right away without checking to see if POLLOUT is available
 	//is it possible to make the list of channels in clients a pointer to the actual channel?
 	std::vector<std::string> chanList = _sender._channels;
 	for (std::vector<std::string>::iterator it = chanList.begin(); it != chanList.end(); it++)
